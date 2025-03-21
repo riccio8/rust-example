fa6536a276b691df9e0176bdc8d8f371daa57b09
0x90 <134144784+riccio8@users.noreply.github.com>
2025-03-21 19:38:38 +0100

    Update main.rs

--- src/SyVibes/src/main.rs
+++ src/SyVibes/src/main.rs
@@ -3,13 +3,12 @@ use sysinfo::System;
 use std::thread;
 use std::time::Duration;
 
-slint::include_modules!();
+slint::include_modules!();  // This macro should define `MainWIndows`. Ensure the `.slint` file is correctly included and compiled.
 
 fn main() {
     let ui = MainWindow::new().unwrap();
     let ui_handle = ui.as_weak();
 
-    // Nuovo thread per aggiornare periodicamente la lista dei processi
     thread::spawn(move || {
         let mut sys = System::new_all();
         loop {
@@ -17,24 +16,19 @@ fn main() {
             let mut list = String::new();
 
             for (pid, process) in sys.processes() {
-                list.push_str(&format!("{:#?} con PID:\t {}\n", process.name(), pid));
+                list.push_str(&format!("{:#?} with PID:\t {}\n", process.name(), pid));
             }
-
-            // Usa invoke_from_event_loop per aggiornare la UI in modo sicuro
             let ui_handle_clone = ui_handle.clone();
             slint::invoke_from_event_loop(move || {
                 if let Some(ui) = ui_handle_clone.upgrade() {
                     ui.set_processes(SharedString::from(list));
-                    dbg!("Aggiornato");
                 }
             })
-            .unwrap(); // Se fallisce, esce con un panic
+            .unwrap(); // If it falls here, it will panic and exit
 
-            thread::sleep(Duration::from_secs(4)); // Aggiorna ogni 4 secondi
+            thread::sleep(Duration::from_secs(4)); 
         }
     });
 
     ui.run().unwrap();
 }
-
-// This macro should define `MainWIndows`. Ensure the `.slint` file is correctly included and compiled.
