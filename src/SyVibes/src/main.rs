8134f031bf2c8af45deafa95037f842c6679b406
0x90 <134144784+riccio8@users.noreply.github.com>
2025-03-22 17:00:38 +0100

    Update main.rs

--- src/SyVibes/src/main.rs
+++ src/SyVibes/src/main.rs
@@ -1,126 +1,171 @@
 use slint::SharedString;
-use sysinfo::{Components, Disks, Networks, System};
-use std::thread;
-use std::time::Duration;
+use sysinfo::{Components, Disks, Networks, System, ProcessesToUpdate,ProcessRefreshKind};
+use tokio::time::{sleep, Duration};
+
 
 slint::include_modules!();  // This macro should define `MainWIndows`
 // in cargo.toml: [package.metadata.winres] windows_subsystem = "Windows"
 
+#[tokio::main]
+async fn main() {
+let ui = MainWindow::new().unwrap();
+let ui_handle = ui.as_weak();
+// ui.window().set_maximized(true);
+tokio::spawn(update_system_info(ui_handle.clone()));
+tokio::spawn(update_process_info(ui_handle.clone()));
+tokio::spawn(update_network_info(ui_handle.clone()));
+tokio::spawn(update_disk_info(ui_handle.clone()));
 
-fn main() {
-    let ui = MainWindow::new().unwrap();
-    let ui_handle = ui.as_weak();
-    // ui.window().set_maximized(true);
+ui.run().unwrap();
+}
 
-    // Clone the ui_handle at the start before moving it into the threads otherwise it won't access it
-    let ui_handle_clone_1 = ui_handle.clone();
-    let ui_handle_clone_2 = ui_handle.clone();
-    let ui_handle_clone_3 = ui_handle.clone();
-    sysinfo::set_open_files_limit(0);
 
-    // Start the first thread for system information.
-    thread::spawn(move || {
-        let mut sys = System::new_all();
-        loop {
-            sys.refresh_all();
-            let mut list = String::new();
-            let mut mem = String::new();
-            let mut cpu = String::new();
-            let mut system = String::new();
+async fn update_system_info(ui_handle: slint::Weak<MainWindow>) {
+    let mut sys = System::new_all();
 
-            for (pid, process) in sys.processes() {
-                list.push_str(&format!("{:#?} with PID:\t {}\n", process.name(), pid));
-            }
+    loop {
+        sys.refresh_all();
 
-            cpu.push_str(&format!("NB CPUs: {}\n", sys.cpus().len()));
-            for (i, cpu_info) in sys.cpus().iter().enumerate() {
-                cpu.push_str(&format!("CPU {} Usage: {}%\n", i, cpu_info.cpu_usage()));
-            }
-            mem.push_str(&format!("Total memory: \t{} bytes \nUsed memory: \t{} bytes \nTotal swap: \t{} bytes \nUsed swap: \t{} bytes",
-             sys.total_memory(), 
-            sys.used_memory(), 
+        let mem = format!(
+            "Total memory: \t{} bytes \nUsed memory: \t{} bytes \nTotal swap: \t{} bytes \nUsed swap: \t{} bytes",
+            sys.total_memory(),
+            sys.used_memory(),
             sys.total_swap(),
-            sys.used_swap()));
+            sys.used_swap()
+        );
 
-            system.push_str(&format!(        "System Information:\n\
+        let cpu = sys
+            .cpus()
+            .iter()
+            .enumerate()
+            .map(|(i, cpu_info)| format!("CPU {} Usage: {}%", i, cpu_info.cpu_usage()))
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        let system = format!(
+            "System Information:\n\
             \tSystem name:           {:#?}\n\
             \tKernel version:        {:#?}\n\
             \tOS version:            {:#?}\n\
-            \tHost name:             {:#?}",
+            \tHost name:             {:#?}\n",
             System::name(),
             System::kernel_version(),
             System::os_version(),
-            System::host_name()));
+            System::host_name()
+        );
 
-            let ui_handle_clone = ui_handle_clone_1.clone();
-            slint::invoke_from_event_loop(move || {
-                if let Some(ui) = ui_handle_clone.upgrade() {
-                    ui.set_processes(SharedString::from(list));
-                    ui.set_memory(SharedString::from(mem));
-                    ui.set_cpu(SharedString::from(cpu));
-                    ui.set_system(SharedString::from(system));
-                }
-            })
-            .unwrap();
-
-            thread::sleep(Duration::from_secs(4)); 
-        }
-    });
-
-    // Start the second thread for network information.
-    thread::spawn(move || {
-        let mut networks = Networks::new_with_refreshed_list();
-        loop {
-            let mut nets = String::new();
-            for (interface_name, data) in &networks {
-                nets.push_str(&format!(
-                    "{}:\n\
-                     \tTotal Data:\n\
-                     \t\tReceived: {} B\n\
-                     \t\tTransmitted: {} B\n\
-                     \tTotal Packets:\n\
-                     \t\tReceived: {}\n\
-                     \t\tTransmitted: {}\n\
-                     \tErrors:\n\
-                     \t\tReceived: {}\n\
-                     \t\tTransmitted: {}\n\
-                     \t\tTransmitted: {}\n\n",
-                    interface_name,
-                    data.total_received(),
-                    data.total_transmitted(),
-                    data.packets_received(),
-                    data.packets_transmitted(),
-                    data.errors_on_received(),
-                    data.errors_on_transmitted(),
-                    data.transmitted(),
-                ));
+        let ui_handle_clone = ui_handle.clone();
+        slint::invoke_from_event_loop(move || {
+            if let Some(ui) = ui_handle_clone.upgrade() {
+                ui.set_memory(SharedString::from(mem));
+                ui.set_cpu(SharedString::from(cpu));
+                ui.set_system(SharedString::from(system));
             }
+        })
+        .unwrap();
 
-            let ui_handle_clone = ui_handle_clone_2.clone();
-            slint::invoke_from_event_loop(move || {
-                if let Some(ui) = ui_handle_clone.upgrade() {
-                    ui.set_network(SharedString::from(nets.clone()));
-                }
-            })
-            .unwrap();
+        sleep(Duration::from_secs(3)).await;
+    }
+}
 
-            thread::sleep(Duration::from_secs(4));
-            networks.refresh(true);
+async fn update_process_info(ui_handle: slint::Weak<MainWindow>) {
+    let mut sys = System::new_all();
+
+    loop {
+        sys.refresh_all();
+        let mut procinfo = String::new();
+
+        for (pid, process) in sys.processes() {
+            procinfo.push_str(&format!(
+                "Process {:#?} (PID: {:#?})\nPath: {:?}\nMemory: {} KB\nCPU: {:.2}%\nStatus: {:?}\n",
+                process.name(),
+                pid,
+                process.exe(),
+                process.memory(),
+                process.cpu_usage(),
+                process.status()
+            ));
+
+            let disk_usage = process.disk_usage();
+            procinfo.push_str(&format!(
+                "Read bytes: new/total => {:?}/{:?}\nWritten bytes: new/total => {:?}/{:?}\n",
+                disk_usage.read_bytes,
+                disk_usage.total_read_bytes,
+                disk_usage.written_bytes,
+                disk_usage.total_written_bytes
+            ));
         }
-    });
 
-    // Start the third thread for disk information.
-    thread::spawn(move || -> ! {
-        let mut disks = Disks::new_with_refreshed_list();
-        
-        let mut components = Components::new_with_refreshed_list();
-        
-        loop {
-            let mut disks_info = String::new();
-            let mut comp = String::new();
+        let ui_handle_clone = ui_handle.clone();
+        slint::invoke_from_event_loop(move || {
+            if let Some(ui) = ui_handle_clone.upgrade() {
+                ui.set_processInfo(SharedString::from(procinfo));
+            }
+        })
+        .unwrap();
 
-            for disk in disks.iter() {
-                disks_info.push_str(&format!(
+        sleep(Duration::from_secs(3)).await;
+
+        sys.refresh_processes_specifics(
+            ProcessesToUpdate::All,
+            true,
+            ProcessRefreshKind::everything().without_cpu().without_disk_usage().without_environ(),
+        );
+    }
+}
+
+
+async fn update_network_info(ui_handle: slint::Weak<MainWindow>) {
+    let mut networks = Networks::new_with_refreshed_list();
+
+    loop {
+        let mut nets = String::new();
+        for (interface_name, data) in &networks {
+            nets.push_str(&format!(
+                "{}:\n\
+                 \tTotal Data:\n\
+                 \t\tReceived: {} B\n\
+                 \t\tTransmitted: {} B\n\
+                 \tTotal Packets:\n\
+                 \t\tReceived: {}\n\
+                 \t\tTransmitted: {}\n\
+                 \tErrors:\n\
+                 \t\tReceived: {}\n\
+                 \t\tTransmitted: {}\n\n",
+                interface_name,
+                data.total_received(),
+                data.total_transmitted(),
+                data.packets_received(),
+                data.packets_transmitted(),
+                data.errors_on_received(),
+                data.errors_on_transmitted()
+            ));
+        }
+
+        let ui_handle_clone = ui_handle.clone();
+        slint::invoke_from_event_loop(move || {
+            if let Some(ui) = ui_handle_clone.upgrade() {
+                ui.set_network(SharedString::from(nets.clone()));
+            }
+        })
+        .unwrap();
+
+        sleep(Duration::from_secs(3)).await;
+        networks.refresh(true);
+    }
+}
+
+
+async fn update_disk_info(ui_handle: slint::Weak<MainWindow>) {
+    let mut disks = Disks::new_with_refreshed_list();
+    let mut components = Components::new_with_refreshed_list();
+
+    loop {
+        let mut disks_info = String::new();
+        let mut comp = String::new();
+
+        for disk in disks.iter() {
+            disks_info.push_str(&format!(
                 "Disk: {:#?}\n\
                 \tType: {:?}\n\
                 \tFile system: {:?}\n\
@@ -138,39 +183,33 @@ fn main() {
                 disk.available_space(),
                 disk.total_space() - disk.available_space(),
                 disk.is_removable(),
-                disk.is_read_only(),
-                ));
-            }
-
-            if components.is_empty() {
-                comp.push_str("No components found.\n");
-            }
-
-            for component in &components {
-                if let Some(temperature) = component.temperature() {
-                    comp.push_str(&format!("{} \t{:#?}°C\n", component.label(), temperature));
-                } else {
-                    comp.push_str(&format!("{} (unknown temperature)\n", component.label()));
-                }
-            }
-
-            // Use the cloned handle in the closure
-            let ui_handle_clone = ui_handle_clone_3.clone();
-            slint::invoke_from_event_loop(move || {
-                if let Some(ui) = ui_handle_clone.upgrade() {
-                    ui.set_disk(SharedString::from(disks_info.clone()));
-                    ui.set_components(SharedString::from(comp.clone()));
-                }
-            })
-            .unwrap();
-
-            thread::sleep(Duration::from_secs(4));
-            disks.refresh(true); 
-            components.refresh(false);
-            
+                disk.is_read_only()
+            ));
         }
-    });
 
-    // Run the UI event loop
-    ui.run().unwrap();
+        if components.is_empty() {
+            comp.push_str("No components found.\n");
+        }
+
+        for component in &components {
+            if let Some(temperature) = component.temperature() {
+                comp.push_str(&format!("{} \t{:#?}°C\n", component.label(), temperature));
+            } else {
+                comp.push_str(&format!("{} (unknown temperature)\n", component.label()));
+            }
+        }
+
+        let ui_handle_clone = ui_handle.clone();
+        slint::invoke_from_event_loop(move || {
+            if let Some(ui) = ui_handle_clone.upgrade() {
+                ui.set_disk(SharedString::from(disks_info.clone()));
+                ui.set_components(SharedString::from(comp.clone()));
+            }
+        })
+        .unwrap();
+
+        sleep(Duration::from_secs(4)).await;
+        disks.refresh(true);
+        components.refresh(false);
+    }
 }
